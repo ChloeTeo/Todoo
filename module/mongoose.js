dc887001161c45a311ba25f85fe8da2947135180
ChloeTeo <teotab08@gmail.com>
2020-10-29 21:27:59 +0800

    index.html modified

--- module/mongoose.js
+++ module/mongoose.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-//const config = require('config');
-var uri = "mongodb://127.0.0.1:27017/TODOO";
+
 var uri2 = "mongodb+srv://UserMe:5d2jFfoEKe1d0tgO@cluster0.mfliq.mongodb.net/ToDoo?retryWrites=true&w=majority"
-//mongoose.connect(`mongodb://${config.get('db.host')}:${config.get('db.port')}/${config.get('db.dbName')}`,
-  //{ useNewUrlParser: true });
-//mongoose.connect(uri2,{useNewUrlParser: true }, { useUnifiedTopology: true });
+
 mongoose.connect(uri2, { useNewUrlParser: true },{ useUnifiedTopology: true })
     .then({
 
@@ -17,19 +14,5 @@ db.once('open',function(){
   console.log("db is connected.")
 })
 
-/*var schema = new mongoose.Schema({
-  date : {type : String}
-}, {collection: "Todo"});
-
-var model1 = mongoose.model('model1', schema);
-
-model1.find()
-  .then((todo) =>{
-    console.log(todo)
-  })
-  .catch((err) =>{
-    console.log(err)
-  })
-  */
   
 module.exports = mongoose;
