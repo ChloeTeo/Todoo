4f7980c2b26103bd99a655a130eb535cc67f4774
ChloeTeo <teotab08@gmail.com>
2020-10-29 20:37:20 +0800

    package.json modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var cors = require("cors");
@@ -17,9 +16,15 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../todopage/build'))),
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'todopage', 'build', 'index.html')) // relative path
+  })
+}
 
 app.use('/', indexRouter);
 app.use('/todo', todoRouter);
