12741be4311462b7c0ea2bc7178d46957df1b967
ChloeTeo <teotab08@gmail.com>
2020-10-31 21:15:50 +0800

    add MONGO_URI

--- module/mongoose.js
+++ module/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-var uri2 = "mongodb+srv://UserMe:5d2jFfoEKe1d0tgO@cluster0.mfliq.mongodb.net/ToDoo?retryWrites=true&w=majority"
+//var uri2 = "mongodb+srv://UserMe:5d2jFfoEKe1d0tgO@cluster0.mfliq.mongodb.net/ToDoo?retryWrites=true&w=majority"
 
-mongoose.connect(uri2, { useNewUrlParser: true },{ useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true },{ useUnifiedTopology: true })
     .then({
 
     })
