d3e23a06a1b72ec3ab58f6ce84380811007085c7
ChloeTeo <teotab08@gmail.com>
2020-10-31 20:07:43 +0800

    app.js modified

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(express.static(path.join(__dirname, '../public')));
   })
 }*/
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build'))),
+  app.use(express.static(path.join(__dirname, '..','build'))),
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build')) // relative path
+    res.sendFile(path.resolve(__dirname, '..','build')) // relative path
   })
 }
 app.use('/', indexRouter);
