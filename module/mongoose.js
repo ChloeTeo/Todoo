2f29f8a206c18426c15f7e4689fc81ca9d367f24
ChloeTeo <teotab08@gmail.com>
2020-10-31 21:24:09 +0800

    add process.env

--- module/mongoose.js
+++ module/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //var uri2 = "mongodb+srv://UserMe:5d2jFfoEKe1d0tgO@cluster0.mfliq.mongodb.net/ToDoo?retryWrites=true&w=majority"
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true },{ useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true },{ useUnifiedTopology: true })
     .then({
 
     })
